e6128e7e1ac8ca95547b57f1aec11f3a6ad031b3
David DeTomaso <davedeto@gmail.com>
2016-03-07 08:11:53 +0000

    Bootstrap the Vim install for a new machine

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 for i in $(find `pwd` -name '*.linux-symlink' );
 do
     filename=$(basename "$i")
@@ -15,3 +16,16 @@ do
     echo "Linking $i to $newfile"
     eval ln -s $i $newfile
 done
+
+# Some more setup for vim
+if [ ! -d ~/.vim ]; then
+    mkdir ~/.vim
+fi
+
+if [ ! -d ~/.vim/swap ]; then
+    mkdir ~/.vim/swap
+fi
+
+if [ ! -d ~/.vim/bundle ]; then
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
