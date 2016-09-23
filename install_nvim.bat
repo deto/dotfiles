3ca0bcd46faa7311ffc8ba4adcdd9c9c49cf93a6
David DeTomaso <davedeto@gmail.com>
2016-09-22 20:44:21 -0700

    modify windows install

--- install_nvim.bat
+++ install_nvim.bat
@@ -1,3 +1,3 @@
-mkdir -p %HOME%\AppData\Local\nvim
-mklink %HOME%\AppData\Local\nvim\init.vim %CD%\vim\init.vim 
-mklink %HOME%\AppData\Local\nvim\ginit.vim %CD%\vim\ginit.vim 
+mkdir %LOCALAPPDATA%\nvim
+mklink %LOCALAPPDATA%\nvim\init.vim %CD%\vim\init.vim 
+mklink %LOCALAPPDATA%\nvim\ginit.vim %CD%\vim\ginit.vim 
