9c3fa539f247f7cc71b390f6347e9bbddb498050
David DeTomaso <davedeto@gmail.com>
2021-01-18 03:16:55 +0000

    Add Jupyter vim shortcuts

---
+++ python/jupyter_custom.js
@@ -0,0 +1,10 @@
+// Configure CodeMirror Keymap
+require([
+  'nbextensions/vim_binding/vim_binding',   // depends your installation
+], function() {
+  // Map jj to <Esc>
+  CodeMirror.Vim.map("jk", "<Esc>", "insert");
+  CodeMirror.Vim.map("H", "^", "normal");
+  CodeMirror.Vim.map("L", "$", "normal");
+});
+
