2c36960db9b4de8c6ffd6fdc5fb0bea337081d31
David DeTomaso <davedeto@gmail.com>
2025-12-14 23:41:25 -0800

    update lazy config

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -4,8 +4,16 @@ return {
     opts = {
       completion = {
         menu = {
+          auto_show = true,
+        },
+        documentation = {
           auto_show = false,
         },
+      },
+      signature = {
+        enabled = false,
+        show_on_trigger_character = false,
+        show_on_insert_on_trigger_character = false,
       },
       keymap = {
         preset = "super-tab",
